760a0146372afa1100620fc5222b75e0fba8eade
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2024-10-24 19:04:55 +0200

    ajout de l'attribut au input du composant Field.

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,57 +1,59 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
-import "./style.scss";
+import './style.scss'
 
 export const FIELD_TYPES = {
   INPUT_TEXT: 1,
   TEXTAREA: 2,
-};
+}
 
 const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
-  let component;
+  let component
   switch (type) {
     case FIELD_TYPES.INPUT_TEXT:
       component = (
         <input
-          type="text"
+          type='text'
           name={name}
           placeholder={placeholder}
-          data-testid="field-testid"
+          data-testid='field-testid'
+          required
         />
-      );
-      break;
+      )
+      break
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid" />;
-      break;
+      component = <textarea name={name} data-testid='field-testid' />
+      break
     default:
       component = (
         <input
-          type="text"
+          type='text'
           name={name}
           placeholder={placeholder}
-          data-testid="field-testid"
+          data-testid='field-testid'
+          required
         />
-      );
+      )
   }
   return (
-    <div className="inputField">
+    <div className='inputField'>
       <span>{label}</span>
       {component}
     </div>
-  );
-};
+  )
+}
 
 Field.propTypes = {
   type: PropTypes.oneOf(Object.values(FIELD_TYPES)),
   name: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string,
-};
- Field.defaultProps = {
-   label: "",
-   placeholder: "",
-   type: FIELD_TYPES.INPUT_TEXT,
-   name: "field-name",
- }
+}
+Field.defaultProps = {
+  label: '',
+  placeholder: '',
+  type: FIELD_TYPES.INPUT_TEXT,
+  name: 'field-name',
+}
 
-export default Field;
+export default Field
